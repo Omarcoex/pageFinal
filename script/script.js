7da8148d4a245e311fdd89414b46b0380e7006a2
Omar Aleiser Alvarez Laverde <omaraleyser@hotmail.com>
2022-10-25 16:41:31 -0500

    Page final

---
+++ script/script.js
@@ -0,0 +1,14 @@
+// window.onload = init;
+//     function init(){
+//         document.querySelectorAll(".menus")[0].addEventListener("click",submenu);
+//         document.querySelector(".menu > ul").style.display = "none"; 
+//     }
+    
+//     function submenu(){
+//         var estado = document.querySelector(".menus > ul").style.display;
+//         if (estado == "none"){
+//             document.querySelector(".menu > ul").style.display = "block";
+//         }else{
+//             document.querySelector(".menus > ul").style.display = "none";            
+//         };
+        
